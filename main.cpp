5b07539417b881c79c8793f0594a5dedf9580721
Simon Risberg <simon@risberg.eu>
2020-10-14 23:01:52 +0200

    First

---
+++ main.cpp
@@ -0,0 +1,107 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+/*******************************************************/ 
+#define SCD(t) scanf("%d",&t)
+#define SCLD(t) scanf("%ld",&t)
+#define SCLLD(t) scanf("%lld",&t)
+#define SCC(t) scanf("%c",&t)
+#define SCS(t) scanf("%s",t)
+#define SCF(t) scanf("%f",&t)
+#define SCLF(t) scanf("%lf",&t)
+#define MEM(a, b) memset(a, (b), sizeof(a))
+#define FOR(i, j, k, in) for (int i=j ; i<k ; i+=in)
+#define RFOR(i, j, k, in) for (int i=j ; i>=k ; i-=in)
+#define REP(i, j) FOR(i, 0, j, 1)
+#define RREP(i, j) RFOR(i, j, 0, 1)
+#define all(cont) cont.begin(), cont.end()
+#define rall(cont) cont.end(), cont.begin()
+#define FOREACH(it, l) for (auto it = l.begin(); it != l.end(); it++)
+#define IN(A, B, C) assert( B <= A && A <= C)
+#define MP make_pair
+#define PB push_back
+#define INF (int)1e9
+#define EPS 1e-9
+#define PI 3.1415926535897932384626433832795
+#define MOD 1000000007
+#define read(type) readInt<type>()
+const double pi=acos(-1.0);
+typedef pair<int, int> PII;
+typedef vector<int> VI;
+typedef vector<string> VS;
+typedef vector<PII> VII;
+typedef vector<VI> VVI;
+typedef map<int,int> MPII;
+typedef set<int> SETI;
+typedef multiset<int> MSETI;
+typedef long int int32;
+typedef unsigned long int uint32;
+typedef long long int int64;
+typedef unsigned long long int  uint64;
+
+template<typename T, typename U> inline void amin(T &x, U y) { if(y < x) x = y; }
+template<typename T, typename U> inline void amax(T &x, U y) { if(x < y) x = y; }
+
+template <typename T> inline void write(T x) {
+	int i = 20;
+	char buf[21];
+	buf[20] = '\n';
+
+	do {
+		buf[--i] = x % 10 + '0';
+		x /= 10;
+	} while(x);
+
+	do {
+		putchar(buf[i]);
+	} while (buf[i++] != '\n');
+}
+
+template <typename T> inline T readInt() {
+	T n=0,s=1;
+	char p=getchar();
+	if(p=='-')s=-1;
+	while((p<'0'||p>'9')&&p!=EOF&&p!='-')p=getchar();
+	if(p=='-')s=-1,p=getchar();
+
+	while(p>='0'&&p<='9') {
+		n = (n<< 3) + (n<< 1) + (p - '0');
+		p=getchar();
+	}
+
+	return n*s;
+}
+/* END OF TEMPLATE */
+
+int main()
+{
+	#ifndef ONLINE_JUDGE
+	/* freopen("input.txt","r",stdin); */
+	// freopen("output.txt","w",stdout);
+	#endif
+
+
+  VS char_grades = {"E","D","C","B","A"};
+  int total_grade = -2;
+  
+  VI max_grades;
+  for(int i=0; i<3; i++) max_grades.PB(read(int));
+
+  VI grades;
+  for(int i=0; i<3; i++) grades.PB(read(int));
+
+  for(int i=2; i>=0; i--) {
+    int grade = grades[i];
+
+    if(grade == max_grades[i]) { total_grade+=2; continue; }
+    else if (grade >= (float)max_grades[i] / 2) { total_grade+=1; break; }
+
+    break;
+  }
+
+  if (total_grade < 0) total_grade = 0;
+  cout << char_grades[total_grade] << endl;
+
+	return 0;
+}
