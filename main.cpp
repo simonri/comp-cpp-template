da6813bc12b019c8f70cac47546e8a9d24ca40c4
Simon Risberg <simon@risberg.eu>
2020-10-17 00:30:23 +0200

    jidji

--- main.cpp
+++ main.cpp
@@ -1,76 +1,166 @@
 #include <bits/stdc++.h>
-
 using namespace std;
 
-#define SCD(t) scanf("%d",&t)
-#define SCLD(t) scanf("%ld",&t)
-#define SCLLD(t) scanf("%lld",&t)
-#define SCC(t) scanf("%c",&t)
-#define SCS(t) scanf("%s",t)
-#define SCF(t) scanf("%f",&t)
-#define SCLF(t) scanf("%lf",&t)
-#define MEM(a, b) memset(a, (b), sizeof(a))
-#define FOR(i, j, k, in) for (int i=j ; i<k ; i+=in)
-#define RFOR(i, j, k, in) for (int i=j ; i>=k ; i-=in)
-#define REP(i, j) FOR(i, 0, j, 1)
-#define RREP(i, j) RFOR(i, j, 0, 1)
-#define all(cont) cont.begin(), cont.end()
-#define rall(cont) cont.end(), cont.begin()
-#define FOREACH(it, l) for (auto it = l.begin(); it != l.end(); it++)
-#define IN(A, B, C) assert( B <= A && A <= C)
-#define MP make_pair
-#define PB push_back
-#define INF (int)1e9
-#define EPS 1e-9
-#define PI 3.1415926535897932384626433832795
-#define MOD 1000000007
-#define read(type) readInt<type>()
-const double pi=acos(-1.0);
-typedef pair<int, int> PII;
-typedef vector<int> VI;
-typedef vector<string> VS;
-typedef vector<PII> VII;
-typedef vector<VI> VVI;
-typedef map<int,int> MPII;
-typedef set<int> SETI;
-typedef multiset<int> MSETI;
-typedef long int int32;
-typedef unsigned long int uint32;
-typedef long long int int64;
-typedef unsigned long long int  uint64;
+#define ll long long
+#define ld long double
+#define ar array
 
-template<typename T, typename U> inline void amin(T &x, U y) { if(y < x) x = y; }
-template<typename T, typename U> inline void amax(T &x, U y) { if(x < y) x = y; }
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
 
-template <typename T> inline void write(T x) {
-	int i = 20;
-	char buf[21];
-	buf[20] = '\n';
+#define vt vector
+#define pb push_back
+#define all(c) (c).begin(), (c).end()
+#define sz(x) (int)(x).size()
 
-	do {
-		buf[--i] = x % 10 + '0';
-		x /= 10;
-	} while(x);
+#define F_OR(i, a, b, s) for (int i=(a); (s)>0?i<(b):i>(b); i+=(s))
+#define F_OR1(e) F_OR(i, 0, e, 1)
+#define F_OR2(i, e) F_OR(i, 0, e, 1)
+#define F_OR3(i, b, e) F_OR(i, b, e, 1)
+#define F_OR4(i, b, e, s) F_OR(i, b, e, s)
+#define GET5(a, b, c, d, e, ...) e
+#define F_ORC(...) GET5(__VA_ARGS__, F_OR4, F_OR3, F_OR2, F_OR1)
+#define FOR(...) F_ORC(__VA_ARGS__)(__VA_ARGS__)
+#define EACH(x, a) for (auto& x: a)
 
-	do {
-		putchar(buf[i]);
-	} while (buf[i++] != '\n');
+template<class T> bool umin(T& a, const T& b) {
+	return b<a?a=b, 1:0;
+}
+template<class T> bool umax(T& a, const T& b) { 
+	return a<b?a=b, 1:0;
+} 
+
+ll FIRSTTRUE(function<bool(ll)> f, ll lb, ll rb) {
+	while(lb<rb) {
+		ll mb=(lb+rb)/2;
+		f(mb)?rb=mb:lb=mb+1; 
+	} 
+	return lb;
+}
+ll LASTTRUE(function<bool(ll)> f, ll lb, ll rb) {
+	while(lb<rb) {
+		ll mb=(lb+rb+1)/2;
+		f(mb)?lb=mb:rb=mb-1; 
+	} 
+	return lb;
 }
 
-template <typename T> inline T readInt() {
-	T n=0,s=1;
-	char p=getchar();
-	if(p=='-')s=-1;
-	while((p<'0'||p>'9')&&p!=EOF&&p!='-')p=getchar();
-	if(p=='-')s=-1,p=getchar();
+template<class A> void read(vt<A>& v);
+template<class A, size_t S> void read(ar<A, S>& a);
+template<class T> void read(T& x) { cin >> x; }
+void read(double& d) {
+	string t;
+	read(t);
+	d=stod(t);
+}
+void read(long double& d) {
+	string t;
+	read(t);
+	d=stold(t);
+}
+template<class H, class... T> void read(H& h, T&... t) {
+	read(h);
+	read(t...);
+}
+template<class A> void read(vt<A>& x) {
+	EACH(a, x) read(a);
+}
+template<class A, size_t S> void read(array<A, S>& x) {
+	EACH(a, x) read(a);
+}
 
-	while(p>='0'&&p<='9') {
-		n = (n<< 3) + (n<< 1) + (p - '0');
-		p=getchar();
+string to_string(char c) {
+	return string(1, c);
+}
+string to_string(bool b) {
+	return b?"true":"false";
+}
+string to_string(const char* s) {
+	return string(s);
+}
+string to_string(string s) {
+	return s;
+}
+string to_string(vt<bool> v) {
+	string res;
+	FOR(sz(v)) res+=char('0'+v[i]);
+	return res;
+}
+
+template<size_t S> string to_string(bitset<S> b) {
+	string res;
+	FOR(S) res+=char('0'+b[i]);
+	return res;
+}
+template<class T> string to_string(T v) {
+	bool f=1;
+	string res;
+	EACH(x, v) {
+		if(!f) res+=' ';
+		f=0;
+		res+=to_string(x);
 	}
-
-	return n*s;
+	return res;
 }
+
+template<class A> void write(A x) {
+	cout << to_string(x);
+}
+template<class H, class... T> void write(const H& h, const T&... t) { 
+	write(h);
+	write(t...);
+}
+void print() {
+	write("\n");
+}
+template<class H, class... T> void print(const H& h, const T&... t) { 
+	write(h);
+	if(sizeof...(t))
+		write(' ');
+	print(t...);
+}
+
+void DBG() {
+	cerr << "]" << endl;
+}
+template<class H, class... T> void DBG(H h, T... t) {
+	cerr << to_string(h);
+	if(sizeof...(t)) cerr << ", ";
+	DBG(t...);
+}
+#ifdef _DEBUG
+#define dbg(...) cerr << "LINE(" << __LINE__ << ") -> [" << #__VA_ARGS__ << "]: [", DBG(__VA_ARGS__)
+#else
+#define dbg(...) 0
+#endif
+
+template<class T> void offset(ll o, T& x) {
+	x+=o;
+}
+template<class T> void offset(ll o, vt<T>& x) {
+	EACH(a, x) offset(o, a);
+}
+template<class T, size_t S> void offset(ll o, ar<T, S>& x) {
+	EACH(a, x) offset(o, a);
+}
+
+mt19937 mt_rng(chrono::steady_clock::now().time_since_epoch().count());
+ll randint(ll a, ll b) {
+	return uniform_int_distribution<ll>(a, b)(mt_rng);
+}
+
+template<class T, class U> void vti(vt<T> &v, U x, size_t n) {
+	v=vt<T>(n, x);
+}
+template<class T, class U> void vti(vt<T> &v, U x, size_t n, size_t m...) {
+	v=vt<T>(n);
+	EACH(a, v) vti(a, x, m);
+}
+
+const int d4i[4]={-1, 0, 1, 0}, d4j[4]={0, 1, 0, -1};
+const int d8i[8]={-1, -1, 0, 1, 1, 1, 0, -1}, d8j[8]={0, 1, 1, 1, 0, -1, -1, -1};
+
 
 int getIndex(vector<char> v, char K) {
 	auto it = find(v.begin(), v.end(), K);
@@ -78,21 +168,48 @@ int getIndex(vector<char> v, char K) {
 	return -1;
 }
 
-int main() {
-	freopen("output.txt", "w", stdout);
-	freopen("input.txt", "r", stdin);
-
-	char alpha[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ" ;
-	vector<char> alphabet(alpha,alpha+sizeof(alpha)-1);
+void solve() {
+	char alpha[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	vector<char> alphabet(alpha, alpha+sizeof(alpha)-1);
 
 	string input;
-	cin>>input;
+	read(input);
 
-	int scrambles=read(int);
+	vt<int> vec, char c;
+	EACH(c, input) {
+		int pos = getIndex(alphabet, c);
+		vec.pb(pos);
+	}
 
-	sort(input.begin(), input.end());
+	int scrambles;
+	read(scrambles);
+	
+	while(scrambles > 0) {
+		for(int i=0; i<(int)vec.size()-1; i++) {
+			if(vec[i] > vec[i+1]) {
+				swap(vec[i], vec[i+1]);
+				scrambles--;
+			} else if(i == (int)vec.size()-2) {
+				scrambles=0;
+			}
+		}
+	}
 
-	cout << getIndex(alphabet, 'C') << endl;
+	int f;
+	EACH(f, vec) cout << alphabet[f] << endl;
+}
+	
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	freopen("output.txt", "w", stdout);
+	freopen("input.txt", "r", stdin);
+	
+	cout << "Hello " << endl;
+
+	solve();
 
 	return 0;
 }
