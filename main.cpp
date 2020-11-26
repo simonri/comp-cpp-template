f9a786106fca387b4eb6dfceec2105cbf7ad6f73
Simon Risberg <simon@risberg.eu>
2020-11-26 23:41:48 +0100

    .

--- main.cpp
+++ main.cpp
@@ -1,188 +1,114 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-#define ll long long
-#define ld long double
-#define ar array
+typedef long long ll;
+typedef pair<int, int> pi;
+typedef vector<int> vi;
+typedef vector<ll> vl;
 
-/* #include <ext/pb_ds/assoc_container.hpp> */
-/* #include <ext/pb_ds/tree_policy.hpp> */
-/* using namespace __gnu_pbds; */
-
-#define vt vector
 #define pb push_back
-#define all(c) (c).begin(), (c).end()
+
+#define rep(i, a, b) for(int i = a; i < (b); ++i)
+#define trav(a, x) for(auto& a : x)
+#define all(x) x.begin(), x.end()
 #define sz(x) (int)(x).size()
 
-#define F_OR(i, a, b, s) for (int i=(a); (s)>0?i<(b):i>(b); i+=(s))
-#define F_OR1(e) F_OR(i, 0, e, 1)
-#define F_OR2(i, e) F_OR(i, 0, e, 1)
-#define F_OR3(i, b, e) F_OR(i, b, e, 1)
-#define F_OR4(i, b, e, s) F_OR(i, b, e, s)
-#define GET5(a, b, c, d, e, ...) e
-#define F_ORC(...) GET5(__VA_ARGS__, F_OR4, F_OR3, F_OR2, F_OR1)
-#define FOR(...) F_ORC(__VA_ARGS__)(__VA_ARGS__)
-#define EACH(x, a) for (auto& x: a)
+#define MOD 1000000007
+#define INF 0x3f3f3f3f 
 
-template<class T> bool umin(T& a, const T& b) {
-	return b<a?a=b, 1:0;
-}
-template<class T> bool umax(T& a, const T& b) { 
-	return a<b?a=b, 1:0;
-} 
+void recursion(vector<vector<bool>> grid, vector<vector<bool>> visited, int currX, int currY, int dirX, int dirY, int iter, vector<pair<int, int>> instructions) {
+	if (iter == 4) {
+		string out;
+		for(pair<int, int> ins : instructions) {
+			if(ins.first == -1 && ins.second == 0) out += "<";
+			else if(ins.first == 1 && ins.second == 0) out += ">";
+			else if(ins.first == 0 && ins.second == 1) out += "v";
+			else if(ins.first == 0 && ins.second == -1) out += "^";
+		}
 
-ll FIRSTTRUE(function<bool(ll)> f, ll lb, ll rb) {
-	while(lb<rb) {
-		ll mb=(lb+rb)/2;
-		f(mb)?rb=mb:lb=mb+1; 
-	} 
-	return lb;
-}
-ll LASTTRUE(function<bool(ll)> f, ll lb, ll rb) {
-	while(lb<rb) {
-		ll mb=(lb+rb+1)/2;
-		f(mb)?lb=mb:rb=mb-1; 
-	} 
-	return lb;
-}
+		int visitedCount = 0;
 
-template<class A> void read(vt<A>& v);
-template<class A, size_t S> void read(ar<A, S>& a);
-template<class T> void read(T& x) { cin >> x; }
-void read(double& d) {
-	string t;
-	read(t);
-	d=stod(t);
-}
-void read(long double& d) {
-	string t;
-	read(t);
-	d=stold(t);
-}
-template<class H, class... T> void read(H& h, T&... t) {
-	read(h);
-	read(t...);
-}
-template<class A> void read(vt<A>& x) {
-	EACH(a, x) read(a);
-}
-template<class A, size_t S> void read(array<A, S>& x) {
-	EACH(a, x) read(a);
-}
+		for(vector<bool> ve : visited) {
+			for(bool vo : ve) {
+				cout << vo;
+				if(vo) visitedCount++;
+			}
+			cout << endl;
+		}
 
-string to_string(char c) {
-	return string(1, c);
-}
-string to_string(bool b) {
-	return b?"true":"false";
-}
-string to_string(const char* s) {
-	return string(s);
-}
-string to_string(string s) {
-	return s;
-}
-string to_string(vt<bool> v) {
-	string res;
-	FOR(sz(v)) res+=char('0'+v[i]);
-	return res;
-}
-
-template<size_t S> string to_string(bitset<S> b) {
-	string res;
-	FOR(S) res+=char('0'+b[i]);
-	return res;
-}
-template<class T> string to_string(T v) {
-	bool f=1;
-	string res;
-	EACH(x, v) {
-		if(!f) res+=' ';
-		f=0;
-		res+=to_string(x);
-	}
-	return res;
-}
-
-template<class A> void write(A x) {
-	cout << to_string(x);
-}
-template<class H, class... T> void write(const H& h, const T&... t) { 
-	write(h);
-	write(t...);
-}
-void print() {
-	write("\n");
-}
-template<class H, class... T> void print(const H& h, const T&... t) { 
-	write(h);
-	if(sizeof...(t))
-		write(' ');
-	print(t...);
-}
-
-void DBG() {
-	cerr << "]" << endl;
-}
-template<class H, class... T> void DBG(H h, T... t) {
-	cerr << to_string(h);
-	if(sizeof...(t)) cerr << ", ";
-	DBG(t...);
-}
-#ifdef _DEBUG
-#define dbg(...) cerr << "LINE(" << __LINE__ << ") -> [" << #__VA_ARGS__ << "]: [", DBG(__VA_ARGS__)
-#else
-#define dbg(...) 0
-#endif
-
-template<class T> void offset(ll o, T& x) {
-	x+=o;
-}
-template<class T> void offset(ll o, vt<T>& x) {
-	EACH(a, x) offset(o, a);
-}
-template<class T, size_t S> void offset(ll o, ar<T, S>& x) {
-	EACH(a, x) offset(o, a);
-}
-
-mt19937 mt_rng(chrono::steady_clock::now().time_since_epoch().count());
-ll randint(ll a, ll b) {
-	return uniform_int_distribution<ll>(a, b)(mt_rng);
-}
-
-template<class T, class U> void vti(vt<T> &v, U x, size_t n) {
-	v=vt<T>(n, x);
-}
-template<class T, class U> void vti(vt<T> &v, U x, size_t n, size_t m...) {
-	v=vt<T>(n);
-	EACH(a, v) vti(a, x, m);
-}
-
-const int d4i[4]={-1, 0, 1, 0}, d4j[4]={0, 1, 0, -1};
-const int d8i[8]={-1, -1, 0, 1, 1, 1, 0, -1}, d8j[8]={0, 1, 1, 1, 0, -1, -1, -1};
-
-int getIndex(vector<char> v, char K) {
-	auto it = find(v.begin(), v.end(), K);
-
-	if(it!=v.end()) return distance(v.begin(), it);
-	return -1;
-}
-
-double slope(const vector<double>& x, const vector<double>& y){
-	size_t n = x.size();
-
-	double avgX = accumulate(x.begin(), x.end(), 0.0) / n;
-	double avgY = accumulate(y.begin(), y.end(), 0.0) / n;
-
-	double numerator = 0.0;
-	double denominator = 0.0;
-
-	for(size_t i=0; i<n; ++i){
-		numerator += (x[i] - avgX) * (y[i] - avgY);
-		denominator += (x[i] - avgX) * (x[i] - avgX);
+		cout << to_string(visitedCount) << " : " << out << endl;
+		return;
 	}
 
-	return numerator / denominator;
+	visited[currY][currX] = true;
+	bool first = true;
+
+	while(!grid[currY + dirY][currX + dirX]) {
+		currX += dirX;
+		currY += dirY;
+		bool curr = grid[currY][currX];
+
+		visited[currY][currX] = true;
+
+		if(first && curr) return;
+		first = false;
+
+		if(currX < 0) currX = 0;
+		if(currY < 0) currY = 0;
+		if(currY > grid.size()) currY = grid.size();
+		if(currX > grid[0].size()) currX = grid[0].size();
+	}
+
+	pair<int, int> nextPair(dirX, dirY);
+	instructions.pb(nextPair);
+
+	recursion(grid, visited, currX, currY, 1, 0, iter+1, instructions);
+	recursion(grid, visited, currX, currY, -1, 0, iter+1, instructions);
+	recursion(grid, visited, currX, currY, 0, 1, iter+1, instructions);
+	recursion(grid, visited, currX, currY, 0, -1, iter+1, instructions);
+
+	return;
+}
+
+void solve() {
+	int height, width, directions;
+	cin >> height >> width >> directions;
+
+	vector<vector<bool>> grid;
+	vector<vector<bool>> visited;
+	int startX = 0, startY = 0;
+
+	for (int i = 0; i < height; i++) {
+		vector<bool> ve;
+		vector<bool> vo;
+		for (int j = 0; j < width; j++) {
+			char cell;
+			cin >> cell;
+			if (cell == '#')
+				ve.pb(true);
+			else
+				ve.pb(false);
+
+			vo.pb(false);
+
+			if (cell == 'O') {
+				startX = j;
+				startY = i;
+			}
+
+			cout << cell;
+		}
+		grid.pb(ve);
+		visited.pb(vo);
+		cout << endl;
+	}
+
+	cout << "Start:" << startX << " " << startY << endl; 
+
+	recursion(grid, visited, startX, startY, 1, 0, 0, {});
+	recursion(grid, visited, startX, startY, -1, 0, 0, {}); 
+	recursion(grid, visited, startX, startY, 0, 1, 0, {});
+	recursion(grid, visited, startX, startY, 0, -1, 0, {});
 }
 
 int main() {
@@ -192,33 +118,7 @@ int main() {
 	freopen("output.txt", "w", stdout);
 	freopen("input.txt", "r", stdin);
 
-	int c, days;
-	read(c);
-	read(days);
+	int _; cin >> _;
 
-	int[] arr[c];
-	for(int i=0; i<3; i++) {
-		int a;
-		read(a);
-
-		arr[a-1] = 1;
-	}
-
-	for(int i=0; i<days; i++) {
-		for(int j=0; j<c; j++) {
-			if(arr[j]) {
-				arr[j-1] = 1;
-				arr[j+1] = 1;
-			}
-		}
-	}
-
-	int count = 0;
-	for(int i=0; i<c; i++) {
-		if(arr[i]) count++;
-	}
-
-	cout << count;
-
-	map<	return 0;
+	solve();
 }
