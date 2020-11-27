45044cf800fb64c3b9b9aa4d58374487800faaec
Simon Risberg <simon@risberg.eu>
2020-11-27 11:15:21 +0100

    .

--- main.cpp
+++ main.cpp
@@ -13,112 +13,33 @@ typedef vector<ll> vl;
 #define all(x) x.begin(), x.end()
 #define sz(x) (int)(x).size()
 
+#define FAST_IO ios::sync_with_stdio(false);cin.tie(0);cout.tie(0)
+#define endl '\n'
+
 #define MOD 1000000007
 #define INF 0x3f3f3f3f 
 
-void recursion(vector<vector<bool>> grid, vector<vector<bool>> visited, int currX, int currY, int dirX, int dirY, int iter, vector<pair<int, int>> instructions) {
-	if (iter == 4) {
-		string out;
-		for(pair<int, int> ins : instructions) {
-			if(ins.first == -1 && ins.second == 0) out += "<";
-			else if(ins.first == 1 && ins.second == 0) out += ">";
-			else if(ins.first == 0 && ins.second == 1) out += "v";
-			else if(ins.first == 0 && ins.second == -1) out += "^";
-		}
-
-		int visitedCount = 0;
-
-		for(vector<bool> ve : visited) {
-			for(bool vo : ve) {
-				cout << vo;
-				if(vo) visitedCount++;
-			}
-			cout << endl;
-		}
-
-		cout << to_string(visitedCount) << " : " << out << endl;
-		return;
-	}
-
-	visited[currY][currX] = true;
-	bool first = true;
-
-	while(!grid[currY + dirY][currX + dirX]) {
-		currX += dirX;
-		currY += dirY;
-		bool curr = grid[currY][currX];
-
-		visited[currY][currX] = true;
-
-		if(first && curr) return;
-		first = false;
-
-		if(currX < 0) currX = 0;
-		if(currY < 0) currY = 0;
-		if(currY > grid.size()) currY = grid.size();
-		if(currX > grid[0].size()) currX = grid[0].size();
-	}
-
-	pair<int, int> nextPair(dirX, dirY);
-	instructions.pb(nextPair);
-
-	recursion(grid, visited, currX, currY, 1, 0, iter+1, instructions);
-	recursion(grid, visited, currX, currY, -1, 0, iter+1, instructions);
-	recursion(grid, visited, currX, currY, 0, 1, iter+1, instructions);
-	recursion(grid, visited, currX, currY, 0, -1, iter+1, instructions);
-
-	return;
-}
-
 void solve() {
-	int height, width, directions;
-	cin >> height >> width >> directions;
+	string word;
+	cin >> word;
 
-	vector<vector<bool>> grid;
-	vector<vector<bool>> visited;
-	int startX = 0, startY = 0;
+	int cSynonyms;
+	cin >> cSynonyms;
 
-	for (int i = 0; i < height; i++) {
-		vector<bool> ve;
-		vector<bool> vo;
-		for (int j = 0; j < width; j++) {
-			char cell;
-			cin >> cell;
-			if (cell == '#')
-				ve.pb(true);
-			else
-				ve.pb(false);
-
-			vo.pb(false);
-
-			if (cell == 'O') {
-				startX = j;
-				startY = i;
-			}
-
-			cout << cell;
-		}
-		grid.pb(ve);
-		visited.pb(vo);
-		cout << endl;
+	vector<string> synonyms;
+	for(int i = 0; i < cSynonyms; i++) {
+		string inp;
+		cin >> inp;
+		synonyms.pb(inp);
+		cout << inp << endl;
 	}
-
-	cout << "Start:" << startX << " " << startY << endl; 
-
-	recursion(grid, visited, startX, startY, 1, 0, 0, {});
-	recursion(grid, visited, startX, startY, -1, 0, 0, {}); 
-	recursion(grid, visited, startX, startY, 0, 1, 0, {});
-	recursion(grid, visited, startX, startY, 0, -1, 0, {});
 }
 
 int main() {
-	ios::sync_with_stdio(0);
-	cin.tie(0);
+	FAST_IO;
 
 	freopen("output.txt", "w", stdout);
 	freopen("input.txt", "r", stdin);
-
-	int _; cin >> _;
 
 	solve();
 }
